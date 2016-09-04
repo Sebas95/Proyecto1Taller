d6572aed01e760772f5aeb438a1a996bff495e40
Giovanni Villalobos <gjosue253@gmail.com>
2016-09-03 19:52:10 -0600

    pequeños cambios state machine

--- MonitorVGA/StateMachine.v
+++ MonitorVGA/StateMachine.v
@@ -25,19 +25,39 @@ module StateMachine(
     input regressive
     );
 	 
-	     always @(state, cnt)
-      case(state)
-      0:  // Color Rojo
-        if (cnt == 100) nextState = 1;
-        else nextState = 0;
-      1:  // Color Ambar
-        if (cnt == 120) nextState = 2;
-        else nextState = 1;
-      2:  // Color Verde
-        if (cnt == 200) nextState = 0;
-        else nextState = 2;
-      default: nextState = 0;
-      endcase
+	// Instantiate the Unit Under Test (UUT)
+	
+	reg enable = 0;
+	reg forward = 0;
+	reg reset;
+	wire [3:0] out;
+	
+	FourBitCounter uut (
+		.out(out), 
+		.enable(enable), 
+		.clk(clk),  
+		.reset(reset),
+		.forward(forward)
+	);
+	 
+	   always @(state, cnt)
+			case(state)
+					0:  // Color Rojo
+					  if (1) nextState = 1;
+					  else nextState = 0;
+					1:  // Color Ambar	
+					  //enable = 1;
+					  if (1) nextState = 2;
+					  else nextState = 1;
+					2:  // Color Verde
+					  if (progressive || regressive)
+							begin
+								if(progressive) nextState = 0;
+								else nextState = 0;
+							end
+					  else nextState = 2;
+					default: nextState = 0;
+			endcase
 
 
 endmodule
