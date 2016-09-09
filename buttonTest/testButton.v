86c1c06c63b153387d70a8bfe40a9085f50fb085
Giovanni Villalobos <gjosue253@gmail.com>
2016-09-09 14:20:09 -0600

    Se agrego proyecto para la prueba del debouncer

---
+++ buttonTest/testButton.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:41:23 09/09/2016 
+// Design Name: 
+// Module Name:    testButton 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module testButton(
+    input wire btn,
+    output butPulse
+    );
+	 
+	 assign btnPulse = btn;
+
+
+endmodule
