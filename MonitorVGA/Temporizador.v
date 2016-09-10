bf1e6decd27ed6956c9d9920e4cee78847398f8a
Giovanni Villalobos <gjosue253@gmail.com>
2016-09-10 00:05:35 -0600

    Se unio el temporizador con la muestra en pantalla de la vga

--- MonitorVGA/Temporizador.v
+++ MonitorVGA/Temporizador.v
@@ -34,51 +34,67 @@ module Temporizador(
 	wire [3:0] sDecimal;
 	
 	reg clk;
+	reg enable = 1;
 	always @(posedge CLK_50MHZ)
 		clk <= ~clk;
 
-
-
    // signal declaration
-	wire [6:0] t_reg;
    wire [9:0] pixel_x, pixel_y;
    wire video_on;
 	//
    wire [3:0]text_on=0;
    wire [3:0] dig0, dig1;
-   reg [1:0] ball_reg=0;
-	wire [3:0]num; //number that is goint to be showed in the 7seg
+	wire [3:0]num;
+   wire [10:0] rom_addr;
+	wire [7:0] font_word;
+
+	//number that is goint to be showed in the 7seg
    //=======================================================
    // instantiation
    //=======================================================
+	
+   font_rom font_unit
+      (.clk(clk), .addr(rom_addr), .data(font_word));	
+		
    // instantiate video synchronization unit
    vga_sync vsync_unit
-      (.clk(clk), .reset(reset), .hsync(hsync), .vsync(vsync),
-       .video_on(video_on), .p_tick(pixel_tick),
-       .pixel_x(pixel_x), .pixel_y(pixel_y));
+      (.clk(clk), 
+		.reset(reset), 
+		.hsync(hsync), 
+		.vsync(vsync),
+      .video_on(video_on), 
+		.p_tick(pixel_tick),
+      .pixel_x(pixel_x),
+		.pixel_y(pixel_y)		
+	);
+	
    // instantiate text module
-   pong_text text_unit
+   vgaWritter text_unit
       (.clk(clk),
-       .pix_x(pixel_x), .pix_y(pixel_y),
-       .dig0(mDecimal), .dig1(mUnit),.dig2(sDecimal),.dig3(sUnit), .ball(ball_reg),
-       .out(num),.text_on(text_on), .text_rgb(rgb));
-	
-	reg enable = 1;
+       .pix_x(pixel_x), 
+		 .pix_y(pixel_y),
+       .dig0(mDecimal), 
+		 .dig1(mUnit),
+		 .dig2(sDecimal),
+		 .dig3(sUnit), 
+		 .text_on(text_on),
+		 .text_rgb(rgb),
+		 .rom_addr(rom_addr),
+		 .font_word(font_word)
+	);
 		
-	 MinutesCounter mc (
-	
-	.enable(enable)  ,  // enable for counter
- 	.clk(CLK_50MHZ)     ,  // clock Input
- 	.reset(reset)   ,  // reset Input
-	.forward(forward), //forward input
-	.incrementSeconds(incrementSeconds),
-	.incrementMinutes(incrementMinutes),
-	.secondsDecimals(sDecimal)     ,  // Output of the counter
-	.secondsUnits(sUnit)     ,  // Output of the counter
-	.minutesDecimals(mDecimal)     ,  // Output of the counter
-	.minutesUnits(mUnit)     ,  // Output of the counter	
-	.finish()
-
+	 MinutesCounter mc (	
+			.enable(enable)  ,  // enable for counter
+			.clk(CLK_50MHZ)     ,  // clock Input
+			.reset(reset)   ,  // reset Input
+			.forward(forward), //forward input
+			.incrementSeconds(incrementSeconds),
+			.incrementMinutes(incrementMinutes),
+			.secondsDecimals(sDecimal)     ,  // Output of the counter
+			.secondsUnits(sUnit)     ,  // Output of the counter
+			.minutesDecimals(mDecimal)     ,  // Output of the counter
+			.minutesUnits(mUnit)     ,  // Output of the counter	
+			.finish()
     );
 
 
